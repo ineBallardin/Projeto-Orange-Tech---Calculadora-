9c480850074953e0c5131834aa2862307a7df443
Karine <ineballardin@gmail.com>
2022-11-25 09:23:19 -0300

    Projeto DIO - Calculadora

--- src/App.js
+++ src/App.js
@@ -1,24 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import Input from './components/Input'
+import Button from './components/Button'
 
-function App() {
+import { Container, Content, Row } from './styles';
+import { useState } from 'react';
+
+const App = () => {
+  const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
+  
+
+  const handleAddNumber = (number) => {
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${number}`)
+  }
+
+  const handleOnClear = () => {
+    setCurrentNumber('0')
+    setFirstNumber('0')
+    setOperation('')
+  }
+
+  const handleSumNumbers = () => {
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber))
+      setCurrentNumber('0')
+      setOperation('+')
+    } else {
+      const sum = Number(firstNumber) + Number(currentNumber)
+      setCurrentNumber(String(sum))
+      setOperation('')
+    }
+  }
+
+  const handleSubNumbers = () => {
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber))
+      setCurrentNumber('0')
+      setOperation('-')
+    } else {
+      const sub = Number(firstNumber) - Number(currentNumber)
+      setCurrentNumber(String(sub))
+      setOperation('')
+    }
+  }
+
+  const handleMultNumbers = () => {
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber))
+      setCurrentNumber('0')
+      setOperation('x')
+    } else {
+      const mult = Number(firstNumber) * Number(currentNumber)
+      setCurrentNumber(String(mult))
+      setOperation('')
+    }
+  }
+
+  const handleDivNumbers = () => {
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber))
+      setCurrentNumber('0')
+      setOperation('/')
+    } else {
+      const div = Number(firstNumber) / Number(currentNumber)
+      setCurrentNumber(String(div))
+      setOperation('')
+    }
+  }
+
+  const handleResult = () => {
+    if(firstNumber !== '0' && operation !== '' && currentNumber !== '0'){
+      switch(operation){
+        case '+':
+          handleSumNumbers()
+          break
+        case '-':
+          handleSubNumbers()
+          break
+        case 'x':
+          handleMultNumbers()
+          break
+        case '/':
+          handleDivNumbers()
+          break
+        default: 
+          break
+      }
+    }
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+        <Input value={currentNumber}/>
+        <Row>
+          <Button label="x" onClick={handleMultNumbers} />
+          <Button label="/" onClick={handleDivNumbers} />
+          <Button label="+" onClick={handleSumNumbers} />
+          <Button label="-" onClick={handleSubNumbers} />
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => handleAddNumber('1')} />
+          <Button label="2" onClick={() => handleAddNumber('2')} />
+          <Button label="3" onClick={() => handleAddNumber('3')} />
+          <Button label="C" onClick={handleOnClear} />
+        </Row>
+        <Row>
+          <Button label="4" onClick={() => handleAddNumber('4')} />
+          <Button label="5" onClick={() => handleAddNumber('5')} />
+          <Button label="6" onClick={() => handleAddNumber('6')} />
+          <Button label="=" onClick={handleResult} />
+        </Row>
+        <Row>
+          <Button label="7" onClick={() => handleAddNumber('7')} />
+          <Button label="8" onClick={() => handleAddNumber('8')} />
+          <Button label="9" onClick={() => handleAddNumber('9')} />
+          <Button label="." onClick={() => handleAddNumber('.')} />
+        </Row>
+      </Content>
+    </Container>
   );
 }
 
---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #00AAF0;
+    color: #FFF;
+    font-size: 24px;
+    font-weight: 700;
+    font-family: 'Roboto';
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+        transition: 0.5s;
+        
+`
